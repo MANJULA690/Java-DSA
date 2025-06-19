6f38de7c9ff5a756dfaf2be3ef657db00ba0cb22
Ramavath Manjula <manjuramavath12@gmail.com>
2025-06-19 15:23:45 +0530

    Arrays

--- Arrays.java
+++ Arrays.java
@@ -134,8 +134,80 @@ public class Arrays {
         System.out.println("max sum is :" + maxSum);
     }
 
+    public static int trappedRainWater(int arr[]) {
+        int n = arr.length;
+        // left max boundary
+        int leftMax[] = new int[n];
+        leftMax[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(arr[i], leftMax[i - 1]);
+        }
+        // right max boundary
+        int rightMax[] = new int[n];
+        rightMax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(arr[i], rightMax[i + 1]);
+        }
+
+        int trappedWater = 0;
+        for (int i = 0; i < n; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += waterLevel - arr[i];
+        }
+        return trappedWater;
+    }
+
+    // best time to buy and sell stocks
+    public static int buyAndSellStocks(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            int sellingPrice = prices[i];
+            if (buyPrice < sellingPrice) {
+                int profit = sellingPrice - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = sellingPrice;
+            }
+        }
+        return maxProfit;
+    }
+
+    public static boolean repeated(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int majorityElement(int nums[]) {
+        int count = 0;
+        int maxCount = Integer.MIN_VALUE;
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                index = nums[i];
+
+            }
+        }
+        return index;
+    }
+
     public static void main(String args[]) {
         int numbers[] = { -1, -2, -3, -4, -4 };
-        kadanes(numbers);
+        int heights[] = { 4, 2, 0, 6, 3, 2, 5 };
+        int prices[] = { 7, 6, 4, 3, 1 };
+        int nums[] = { 1, 2, 3, 4, 1, 2, 2, 2 };
+        System.out.println(majorityElement(nums));
     }
 }

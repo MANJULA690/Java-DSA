e42ec7a48f4c7c129fb4f73257df134ab94afc24
Ramavath Manjula <manjuramavath12@gmail.com>
2025-06-20 08:31:47 +0530

    sorting

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -72,9 +72,73 @@ public class SortingAlgorithms {
         }
     }
 
+    // practice
+    // bubble sort in reverse order
+    public static void bubbleSortRev(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // selection sort in reverse order
+    public static void selectionSortRev(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int maxPos = i;
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (arr[maxPos] < arr[j]) {
+                    maxPos = j;
+                }
+            }
+            int temp = arr[maxPos];
+            arr[maxPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // insertionSort in reverse order
+    public static void insertionSortRev(int arr[]) {
+        for (int i = 1; i < arr.length - 1; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] < curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+    }
+
+    // count sort in reverse order
+    public static void countSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+        for (int i = count.length - 1; i >= 0; i--) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int nums[] = { 1, 5, 10, 3, 15, 999 };
-        countingSort(nums);
+        int nums[] = { 5, 1, 3, 4, 2 };
+        countSort(nums);
         printArray(nums);
     }
 }

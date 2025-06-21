25a8168b1c1b23cd53b3a5a131b86dde5c5e26d5
Ramavath Manjula <manjuramavath12@gmail.com>
2025-06-22 00:33:59 +0530

    bit manipulation

--- Strings.java
+++ Strings.java
@@ -45,10 +45,28 @@ public class Strings {
         return largest;
     }
 
+    public static String compress(String str) {
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if (count > 1) {
+                sb.append(count.toString());
+            }
+        }
+        return sb.toString();
+    }
+
     public static void main(String args[]) {
         // System.out.println(isPalindrome("racecar"));
         // System.out.println(shortestPath("NS"));
-        String fruits[] = { "apple", "mango", "banana" };
-        System.out.println(largestStr(fruits));
+        // String fruits[] = { "apple", "mango", "banana" };
+        // System.out.println(largestStr(fruits));
+        String str = "abcd";
+        System.out.println(compress(str));
     }
 }

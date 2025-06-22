4e126a11d53e0c825c2c51faead41d2f9bc7a3af
Ramavath Manjula <manjuramavath12@gmail.com>
2025-06-22 06:30:14 +0530

    bit manipulation

---
+++ BitManipulation.java
@@ -0,0 +1,65 @@
+public class BitManipulation {
+
+    public static void oddOrEven(int n) {
+        int bitMask = 1;
+        if ((n & bitMask) == 0) {
+            System.out.println("even number");
+        } else {
+            System.out.println("odd number");
+        }
+    }
+
+    public static int getIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        if ((n & bitMask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public static int setIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        return n | bitMask;
+    }
+
+    public static int clearIthBit(int n, int i) {
+        int bitMask = ~(1 << i);
+        return n & bitMask;
+    }
+
+    public static int updateIthBit(int n, int i, int newBit) {
+        // if (newBit == 0) {
+        // return clearIthBit(n, i);
+        // } else {
+        // return setIthBit(n, i);
+        // }
+        n = clearIthBit(n, i);
+        int bitMask = newBit << i;
+        return n | bitMask;
+
+    }
+
+    public static int clearIBits(int n, int i) {
+        int bitMask = (~0) << i;
+        return n & bitMask;
+    }
+
+    public static int countSetBits(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        // oddOrEven(3);
+        // oddOrEven(14);
+        // System.out.println(clearIBits(15, 2));
+        System.out.println(countSetBits(3));
+    }
+}

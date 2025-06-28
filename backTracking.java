69ec56fd3e98cc8e999df4cba33cd0ad9c217d47
Ramavath Manjula <manjuramavath12@gmail.com>
2025-06-28 09:25:08 +0530

    backtracking

--- backTracking.java
+++ backTracking.java
@@ -10,6 +10,15 @@ public class backTracking {
         arr[i] = arr[i] - 2;
     }
 
+    public static void findSubsets(String str, int i, String ans) {
+        if (i == str.length()) {
+            System.out.println(ans);
+            return;
+        }
+        findSubsets(str, i + 1, ans + str.charAt(i));
+        findSubsets(str, i + 1, ans);
+    }
+
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -17,9 +26,25 @@ public class backTracking {
         System.out.println();
     }
 
+    public static void findPermutations(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            findPermutations(newStr, ans + curr);
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = new int[5];
-        changeArr(arr, 0, 1);
-        printArr(arr);
+        // changeArr(arr, 0, 1);
+        // printArr(arr);
+        String str = "abc";
+        // findSubsets(str, 0, "");
+        findPermutations(str, "");
     }
 }

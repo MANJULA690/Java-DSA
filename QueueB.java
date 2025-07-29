d1a9c011cf5014a0b9f48b32c20924bea03559a7
Ramavath Manjula <manjuramavath12@gmail.com>
2025-07-29 22:18:54 +0530

    Binary trees

--- QueueB.java
+++ QueueB.java
@@ -121,13 +121,13 @@ public class QueueB {
     }
 
     public static void reverse(Queue<Integer> q) {
-        Stack<Integer> s = new Stack<>();
-        while (!q.isEmpty()) {
-            s.push(q.remove());
-        }
-        while (!s.isEmpty()) {
-            q.add(s.pop());
-        }
+        // Stack<Integer> s = new Stack<>();
+        // while (!q.isEmpty()) {
+        // s.push(q.remove());
+        // }
+        // while (!s.isEmpty()) {
+        // q.add(s.pop());
+        // }
     }
 
     // Queue q = new Queue(

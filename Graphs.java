d58c983aff164570c9bed19a20c50e24ccca49d0
Ramavath Manjula <manjuramavath12@gmail.com>
2025-08-26 08:16:13 +0530

    Graphs - part 3

--- Graphs.java
+++ Graphs.java
@@ -161,29 +161,75 @@ public class Graphs {
         return false;
     }
 
+    public static void calcIndeg(ArrayList<Edge>[] graph, int[] indeg) {
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph[i].size(); j++) {
+                Edge e = graph[i].get(j);
+                indeg[e.dest]++;
+            }
+        }
+    }
+
+    public static void topSort(ArrayList<Edge>[] graph) {
+        int[] indeg = new int[graph.length];
+        calcIndeg(graph, indeg);
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < indeg.length; i++) {
+            if (indeg[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            System.out.print(curr + " ");
+            for (int i = 0; i < graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+                indeg[e.dest]--;
+                if (indeg[e.dest] == 0) {
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+
+    public static void printAllPaths(ArrayList<Edge>[] graph, int src, int dest, String path) {
+        if (src == dest) {
+            System.out.println(path + dest);
+            return;
+        }
+        for (int i = 0; i < graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            printAllPaths(graph, e.dest, dest, path + src);
+        }
+    }
+
     public static void createGraph(ArrayList<Edge>[] graph, int V) {
         for (int i = 0; i < V; i++) {
             graph[i] = new ArrayList<>();
         }
 
         // graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 3));
 
-        graph[1].add(new Edge(1, 0));
+        // graph[1].add(new Edge(1, 0));
         // graph[1].add(new Edge(1, 3));
 
         // graph[2].add(new Edge(2, 0));
         graph[2].add(new Edge(2, 3));
 
-        graph[3].add(new Edge(3, 0));
+        graph[3].add(new Edge(3, 1));
         // graph[3].add(new Edge(3, 4));
 
-        // graph[4].add(new Edge(4, 2));
-        // graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
     }
 
     public static void main(String[] args) {
-        int V = 5;
+        int V = 6;
         ArrayList<Edge>[] graph = new ArrayList[V];
         createGraph(graph, V);
         // bfs(graph);
@@ -193,6 +239,8 @@ public class Graphs {
         // System.out.println(hasPath(graph, 0, 7, new boolean[V]));
         // System.out.println(detectCycle(graph));
         // System.out.println(isBipartite(graph));
-        System.out.println(hasCycle(graph));
+        // System.out.println(hasCycle(graph));
+        // topSort(graph);
+        printAllPaths(graph, 5, 1, "");
     }
 }
